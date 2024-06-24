52a5cc7d46167bb084db963e8386cbbba6790570
Noor-Abdelmuneim <nooralbeaty18@gmail.com>
2024-06-24 15:10:23 +0300

    fix: edit buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,43 @@
 import { Link } from "react-router-dom";
 import building from "./Assets/Building.png";
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { blueGrey
+ } from '@mui/material/colors';
+
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(blueGrey
+[500]),
+  backgroundColor: blueGrey
+[500],
+  '&:hover': {
+    backgroundColor: blueGrey
+[700],
+  },
+}));
+
 
 const Hero = () => {
   return (
     <>
       <div className="hero-container">
         <div className="text-container">
-          <h1>Invest in Iraq's Future: Discover Startup Opportunities</h1>
-          <br />
+          <h1 className={"header-text"}>Invest in Iraq's Future: Discover Startup Opportunities</h1>
           <p>
             Iraq Startup Hub connects innovative startups with visionary
             investors seeking impactful opportunities in Iraq's burgeoning
             market.
           </p>
-          <br />
           <Link to="/startups">
-            <button className={"button-18"}>View More</button>
+            <Stack direction="row">
+              <ColorButton variant="contained" sx={{
+                borderRadius: "30px",
+                paddingLeft: "20px",
+                paddingRight: "20px",
+                // margin:"100px",
+              }}>View More</ColorButton>
+            </Stack>
           </Link>
         </div>
         <img src={building} alt="building" className="hero-image" />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import logo from "../photos/money.png";
+import { Button, Stack } from "@mui/material";
 
 const Navbar = () => {
   const location = useLocation();
@@ -25,34 +26,55 @@ const Navbar = () => {
           <Link to="/">Home</Link>
         </li>
         <li
-          className={`nav-item ${
-            location.pathname.startsWith("/startups") ? "active" : ""
-          }`}
+          className={`nav-item ${location.pathname.startsWith("/startups") ? "active" : ""
+            }`}
         >
           <Link to="/startups">Start-ups</Link>
         </li>
         <li
-          className={`nav-item ${
-            location.pathname.startsWith("/investors") ? "active" : ""
-          }`}
+          className={`nav-item ${location.pathname.startsWith("/investors") ? "active" : ""
+            }`}
         >
           <Link to="/investors">Investors & Incubators</Link>
         </li>
         <li
-          className={`nav-item ${
-            location.pathname === "/contact" ? "active" : ""
-          }`}
+          className={`nav-item ${location.pathname === "/contact" ? "active" : ""
+            }`}
         >
           <Link to="/contact">Contact us</Link>
         </li>
       </ul>
       <div className={`nav-btns ${isOpen ? "active" : ""}`}>
+      <Stack spacing={1.5} direction="row">
         <Link to="/loginpage">
-          <button className={"button-18"}>Login</button>
+          <Button variant="contained" sx={{
+            fontFamily:"",
+            borderRadius: "30px",
+            paddingLeft: "20px",
+            paddingRight: "20px",
+            backgroundColor: "#0d47a1",
+            color: "#ffffff",
+            boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.1)",
+            '&:hover': {
+              backgroundColor: "#1565c0",
+              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+            },
+          }}>Login</Button>
         </Link>
         <Link to="/signup">
-          <button className={"button-18"}>Sign Up</button>
+          <Button variant="outlined" sx={{
+            borderRadius: "30px",
+            paddingLeft: "20px",
+            paddingRight: "20px",
+            color: "#black",
+            boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.1)",
+            '&:hover': {
+              backgroundColor: "#007bff3",
+              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+            },
+          }}>Sign Up</Button>
         </Link>
+        </Stack>
       </div>
     </nav>
   );
