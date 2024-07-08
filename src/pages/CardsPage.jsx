eda62a409d0cbb746ee42da7783906a67533d2ed
Noor-Abdelmuneim <nooralbeaty18@gmail.com>
2024-07-08 13:27:16 +0300

    startups page edit

--- src/pages/CardsPage.jsx
+++ src/pages/CardsPage.jsx
@@ -5,6 +5,7 @@ const CardsPage = () => {
   return (
     <div className="startups-container">
       <h1 className="header-cards">Startups In Iraqi Market</h1>
+      <p className="cards-p">Discover a vibrant community of Iraqi startups driving forward innovation across various sectors. These entrepreneurial ventures are pioneering solutions in technology, energy, healthcare, education, and e-commerce. Each startup brings unique perspectives and ambitions, contributing to Iraq's evolving business landscape and promising future.</p>
       <div className="cards">
         <Card
           name="Company name"
