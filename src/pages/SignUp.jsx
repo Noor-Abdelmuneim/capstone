04375d190021d0da32392796d412b930619f6877
Noor-Abdelmuneim <nooralbeaty18@gmail.com>
2024-07-22 14:05:23 +0300

    a button for upload logo added in signup page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -10,7 +10,21 @@ import {
   FormControl,
   FormHelperText,
 } from "@mui/material";
-import { Margin } from "@mui/icons-material";
+import { styled } from '@mui/material/styles';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
 
 const RegistrationForm = () => {
   const [companyName, setCompanyName] = useState("");
@@ -62,10 +76,13 @@ const RegistrationForm = () => {
         style={{
           color: "#1769aa",
           fontSize: "1.8rem",
+          display:"flex",
+          justifyContent:"center",
           margin: "20px 0",
         }}
+        className="form-title"
       >
-        <b>Startup Company Registration Form</b>
+        <b className="form-title">Startup Company Registration Form</b>
       </Typography>
       <form onSubmit={handleSubmit}>
         <Grid container spacing={2}>
@@ -127,16 +144,29 @@ const RegistrationForm = () => {
                 <FormHelperText>This field is required</FormHelperText>
               )}
             </FormControl>
+            <Button
+              component="label"
+              role={undefined}
+              variant="contained"
+              tabIndex={-1}
+              startIcon={<CloudUploadIcon />}
+              style={{
+                margin: "20px 0",
+                textTransform: "none",
+                fontFamily: "Arial, Helvetica, sans-serif",
+                fontWeight: "bold",
+              }}
+            >
+              Upload Company Logo
+              <VisuallyHiddenInput type="file" required />
+            </Button>
           </Grid>
-          <div></div>
-
         </Grid>
         <Button
           type="submit"
           variant="contained"
           color="primary"
           style={{
-            margin: "30px 0",
             textTransform: "none",
             fontFamily: "Arial, Helvetica, sans-serif",
             fontWeight: "bold",
