7f3a2450aca3e8e7f6b0db6f545c97ff667f737c
Noor-Abdelmuneim <nooralbeaty18@gmail.com>
2024-07-24 15:47:12 +0300

    test

--- src/pages/SingleCard.jsx
+++ src/pages/SingleCard.jsx
@@ -1,28 +1,47 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import companypic from "../components/Assets/Münster,_LVM,_Bürogebäude_--_2013_--_5149-51.jpg"
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import companypic from '../components/Assets/Münster,_LVM,_Bürogebäude_--_2013_--_5149-51.jpg';
 
-const SingleCard = ({ companyName, about, email, url, id }) => {
-    return (
-        <div className="single-card" id={id}>
-            <div className="single-card-content">
-                <div className="single-card-title">
-                    <h1>company name{companyName}</h1>
-                </div>
-                <div className="single-card-img">
-                    <img src={companypic} width="40%" alt="Company Logo" />
-                </div>
-                <h3>About The Company:</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore neque odit illo ipsum exercitationem facere sint quos, a necessitatibus iusto recusandae rerum iste voluptates ut, natus repellendus est! Atque, facere?{about}</p>
-                <a href={`mailto:${email}`} className="email-link">
-                    <p className="email">email{email}</p>
-                </a>
-                <Link to={url} target="_blank" rel="noopener noreferrer">
-                    <button className="visit-button">Visit Website</button>
-                </Link>
-            </div>
+const SingleCard = ({ id }) => {
+  const [companyData, setCompanyData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://x8ki-letl-twmt.n7.xano.io/api:1cUXcVZQ/startups/${id}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setCompanyData(data); 
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, [id]);
+
+
+  return (
+    <div className="single-card" id={id}>
+      <div className="single-card-content">
+        <div className="single-card-title">
+          <h1>{companyData.companyName}</h1>
         </div>
-    );
+        <div className="single-card-img">
+          <img src={companypic} width="40%" alt="Company Logo" />
+        </div>
+        <h3>About The Company:</h3>
+        <p>{companyData.about}</p>
+        <a href={`mailto:${companyData.email}`} className="email-link">
+          <p className="email">{companyData.email}</p>
+        </a>
+        <Link to={companyData.url} target="_blank" rel="noopener noreferrer">
+          <button className="visit-button">Visit Website</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SingleCard;
