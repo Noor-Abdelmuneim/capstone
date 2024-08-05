3213074d130eaefbaed0caab10617f94c87f9dbc
Noor-Abdelmuneim <157888524+Noor-Abdelmuneim@users.noreply.github.com>
2024-08-05 08:40:42 +0300

    fix SingleCard page

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -33,20 +33,20 @@ const SingleCard = () => {
   if (error) return <Loading />;
   if (!companyData) return <p>No data available</p>;
 
-  const { Company_Name, About, email, website, business_nature, img } =
+  const { company_name, about, email, website, business_nature, company_logo } =
     companyData;
 
   return (
     <div className="single-card" id={id}>
       <div className="single-card-content">
         <div className="single-card-title">
-          <h1 className="header-text"> {Company_Name}</h1>
+          <h1 className="header-text"> {company_name}</h1>
         </div>
         <div className="single-card-img">
-          <img src={img} alt="Company Logo" />
+          <img src={company_logo.url} alt="Company Logo" />
         </div>
         <h2>About The Company:</h2>
-        <p>{About}</p>
+        <p>{about}</p>
         <p className="business-nature">{business_nature}</p>
         <a href={`mailto:${email}`} className="email-link">
           <p className="email">{email}</p>
